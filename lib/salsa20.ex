89a329add74f5f267f4a88a37ade1502cd5b8c59
Matt Miller <mwm@fastmail.com>
2016-03-02 07:00:08 +0200

    Improve alignment for readability.

--- lib/salsa20.ex
+++ lib/salsa20.ex
@@ -50,9 +50,9 @@ defmodule Salsa20 do
 
   @doc false
   def rowround([y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15]) do
-    [z0, z1, z2, z3] = quarterround([y0, y1, y2, y3])
-    [z5, z6, z7, z4] = quarterround([y5, y6, y7, y4])
-    [z10, z11, z8, z9] = quarterround([y10, y11, y8, y9])
+    [ z0,  z1,  z2,  z3] = quarterround([ y0,  y1,  y2,  y3])
+    [ z5,  z6,  z7,  z4] = quarterround([ y5,  y6,  y7,  y4])
+    [z10, z11,  z8,  z9] = quarterround([y10, y11,  y8,  y9])
     [z15, z12, z13, z14] = quarterround([y15, y12, y13, y14])
 
     [z0,z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12,z13,z14,z15]
@@ -60,10 +60,10 @@ defmodule Salsa20 do
 
   @doc false
   def columnround([x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15]) do
-    [y0, y4, y8, y12] = quarterround([x0, x4, x8, x12])
-    [y5, y9, y13, y1] = quarterround([x5, x9, x13, x1])
-    [y10, y14, y2, y6] = quarterround([x10, x14, x2, x6])
-    [y15, y3, y7, y11] = quarterround([x15, x3, x7, x11])
+    [ y0,  y4,  y8, y12] = quarterround([ x0,  x4,  x8, x12])
+    [ y5,  y9, y13,  y1] = quarterround([ x5,  x9, x13,  x1])
+    [y10, y14,  y2,  y6] = quarterround([x10, x14,  x2,  x6])
+    [y15,  y3,  y7, y11] = quarterround([x15,  x3,  x7, x11])
 
     [y0,y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14,y15]
   end
