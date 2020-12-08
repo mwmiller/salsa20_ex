effab64f069ade51789fbda4826d1d0d182109d8
Matthew Miller <matt.miller@nfiindustries.com>
2020-12-08 08:51:01 -0600

    Update deps

--- mix.exs
+++ mix.exs
@@ -22,9 +22,8 @@ defmodule Salsa20.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev},
-      {:credo, "~> 1.0", only: [:dev, :test]}
+      {:earmark, "~> 1.4", only: :dev},
+      {:ex_doc, "~> 0.23", only: :dev},
     ]
   end
 
